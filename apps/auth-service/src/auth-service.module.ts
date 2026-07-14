b04e48eac80421d7a09a402d0c3a5dd5819d9094
Mrfroxi <pilutikas340@gmail.com>
2026-07-15 00:56:46 +0300

    feat: add prisma service composition

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
